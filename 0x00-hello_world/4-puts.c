49f5503fcc74cfd69ae1149437fa19d6601425ba
fridahnamudu <fridah.namudu@gmail.com>
2022-03-13 23:20:57 +0300

    add them

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
+
 /**
  * main - Entry point of the program
  *
  * Return:  0 if the program is run successfully else return 1
  */
+
 int main(void)
 {
     puts("\"Programing is like building a multilingual puzzle");
